b91c3a3e29e08decdc570da6094788a941db602d
ArielGarciaRenteria <a291641@uach.mx>
2017-11-27 21:49:23 -0700

    Primer Commit
    Se agregan los metodos necesarios

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -8,41 +8,64 @@ import { ClientService } from '../../services/client/client.service';
 })
 export class ClientsComponent implements OnInit {
 
-  constructor(private clientService: ClientService) {
+  users: Array<any>;
 
-    this.users = this.clientService.clients;
+  user: { name: "", lastName: "", email: "" };
+  action: string = "default";
+
+  constructor(private clientService: ClientService) {
 
   }
 
   ngOnInit() {
+    this.onFind();
   }
 
-  onSave(user){
-  if (this.action == "edit"){
-  }
-  if (this.action == "create"){
-    this.users.push(user);
-  }
+  onSelect(user){
+    this.user= JSON.parse(JSON.stringify(user));
   }
 
   onCreate(){
-  this.user = {name: "", lastName: "", email: ""};
-  this.action = "create";
-}
-
-  onDelete(index){
-    this.users.splice(index, 1);
-    }
-
-  onEdit(user){
-    this.user = user;
-    this.action = "edit";
+    this.user = {name:"", lastName:"", email:""};
+    this.action = "create";
   }
 
+  onEdit(user){
+    this.user= JSON.parse(JSON.stringify(user));
+    this.action= "edit";
+  }
 
+  onFind() {
+    this.clientService.find().subscribe((res: any) => {
+      this.users = res.body;
+    });
+  }
 
-  users: Array<any>;
+  onSave(user) {
+    if (this.action == "edit") {
+      this.clientService.updateOne(user).subscribe((res: any) => {
+        this.onFind();
+      });
+    }
+    if (this.action == "create") {
+      this.clientService.insertOne(user).subscribe((res: any) => {
+        this.onFind();
+      });
+    }
+  }
 
-  user: {name: "", lastName: "", email: ""};
-  action: string = "default";
+  onDelete(id) {
+    this.clientService.deleteOne(id).subscribe((res: any) => {
+      this.onFind();
+    });
+  }
+
+  updateclients(search){
+
+    console.log(search);
+        this.clientService.findbyname(search).subscribe((res:any) => {
+        this.users = res.body;
+        console.log(res.body);
+        });
+      }
 }
