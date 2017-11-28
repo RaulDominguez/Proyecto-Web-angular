b5082cd26385d57f7a5dcab63c06426e758a3095
RaulDominguez <a291452@uach.mx>
2017-11-28 14:27:43 -0700

    Funcionalidad de Inventory
    Se agrego la barra de busqueda de producto y métodos en el componente

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -10,18 +10,25 @@ export class InventoryComponent implements OnInit {
 
   constructor(private inventoryService: InventoryService) {
 
-    this.items = this.inventoryService.items;
+    //this.items = this.inventoryService.items;
 
   }
+
+  items: Array<any>;
+
+  item: {name: "", brand: "", provider: "", quantity: "", price: ""};
+  action: string = "default";
 
   ngOnInit() {
     this.onFind();
   }
 
+//Metodos
   onCreate(){
     this.item = {name: "", brand: "", provider: "", quantity: "", price: ""};
     this.action = "create";
   }
+
   onSave(user){
   if (this.action == "edit"){
     this.inventoryService.updateOne(user).subscribe((res:any) => {
@@ -51,8 +58,4 @@ export class InventoryComponent implements OnInit {
   });
 }
 
-  items: Array<any>;
-
-  item: {name: "", brand: "", provider: "", quantity: "", price: ""};
-  action: string = "default";
 }
