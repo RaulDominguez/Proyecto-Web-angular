8f5a9a0c5c254fbb1260297a36e00f1053eef315
ArielGarciaRenteria <a291641@uach.mx>
2017-11-28 13:15:48 -0700

    Quinto commit
    Se actualizo el servicio sales

--- src/app/services/sales/sales.service.ts
+++ src/app/services/sales/sales.service.ts
@@ -1,42 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
+
 export class SalesService {
-
-  constructor() { }
-
+  constructor( private http:HttpClient ) { }
   sale: any = {
-  client: {name: "", lastName: "", email: ""},
-  address: {street:"", city: "", zip: ""},
-  items: [],
-};
+    client: {name: "", lastName: "", email: ""},
+    address: {street:"", city: "", zip: ""},
+    items: [],
+  };
+  sales: any = [ ];
 
-  sales: any = [
-  {
-    order: 223344,
-    date:new Date(),
-    total: 44553,
-    client: {name: "Victor Hugo", lastName: "Aguilar Parra", email: "victor@email.com"},
-    address: {street:"San pancho 3432", city: "Chihuahua", zip: "446744"},
-    items: [
-      {id:1, name: "Aceite", brand: "Cada Dia", provider: "AGD", quantity: "10", price: "50"},
-      {id:2, name: "Arroz", brand: "Molinos", provider: "Adeco", quantity: "100", price: "20"},
-      {id:3, name: "Azucar", brand: "Chango", provider: "Adeco", quantity: "20", price: "25"}
-    ],
-  },
-  {
-    order: 776655,
-    date:new Date(),
-    total: 345,
-    client: {name: "Juan Manuel ", lastName: "Medrano", email: "Juan@email.com"},
-    address: {street:"San pancho 3432", city: "Chihuahua", zip: "446744"},
-    items: [
-      {id:1, name: "Aceite", brand: "Cada Dia", provider: "AGD", quantity: "10", price: "50"},
-      {id:2, name: "Arroz", brand: "Molinos", provider: "Adeco", quantity: "100", price: "20"},
-      {id:3, name: "Arroz", brand: "Molinos", provider: "Adeco", quantity: "100", price: "20"},
-      {id:4, name: "Azucar", brand: "Chango", provider: "Adeco", quantity: "20", price: "25"}
-    ],
+  find(){
+    return this.http.get('/api/sales');
   }
-];
 
+  insertOne(obj){
+    return this.http.post('/api/sales', obj);
+  }
+
+  deleteOne(id){
+    return this.http.delete('/api/sales/' + id);
+  }
+
+  updateOne(obj){
+    return this.http.put('/api/sales', obj);
+  }
 }
